bbd456b9679611af878570840e96e1e381018def
camsn0w <camhconnell@gmai.com>
2019-04-17 20:24:37 -0600

    Hello Worl

--- src/Generator.java
+++ src/Generator.java
@@ -4,37 +4,51 @@
  * 4/9/19
  */
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class Generator
 {
 		//public String fileName = "/RecursionPlate.txt";
 		//public String fileName = "/home/camsn0w/Documents/Java-PrepGenerator/src/RecursionPlate.txt";
-		public static String fileName = "input/RecursionPlate.txt";
+		public static String fileName = "RecursionPlate.txt";
+
 		public static void main(String []args)
 		{
 			//Generator g = new Generator();
 			//g.read();
-			read();
+			File temp = read(fileName);
+			randomizer(temp);
+
+
+
+			/*try {
+				Scanner filez = new Scanner(temp);
+				while (filez.hasNext())
+				{
+					System.out.println(filez.nextLine());
+				}
+			}
+			catch (IOException e)
+			{
+				System.out.println("Na");
+			}*/
+
 		}
 
-		public Generator()
-		{
 
-		}
-		public static File read()
+		public static File read(String fileName)
 		{
 
 				try {
 					Scanner inputer = new Scanner(new File(fileName));
 					//File f = new File(String.valueOf(inputer));
 
-					while (inputer.hasNext()) {
+					/*while (inputer.hasNext()) {
 						System.out.println(inputer.nextLine());
 
-					}
+					}*/
+					//File f = new File(fileName);
 					return new File(fileName);
 				}
 				catch (IOException e)
@@ -45,7 +59,69 @@ public class Generator
 
 		}
 
+		public static void randomizer(File F)
+		{
+			if (F.length() == 0)
+			{
+				System.out.println("File is empty");
+				return;
+			}
+			try
+			{
+				Scanner lookThrough = new Scanner(F);
+				BufferedWriter writer = new BufferedWriter(new FileWriter("newestOutput.txt"));
+				while (lookThrough.hasNext())
+				{
+					String temp = lookThrough.nextLine();
+					if(temp.contains("#"))
+					{
+						String temp2 = "";
+						for (int i = 0; i < temp.length(); i++)
+						{
+							if (temp.charAt(i) != '#')
+							{
+								temp2 += temp.charAt(i);
+							}
+							else
+							{
+								temp2 += chunkReplace(temp.charAt(i+1));
+								i = i + 2;
+							}
 
+						}
+						temp = temp2;
+					}
+					writer.write(temp + "\n");
+
+				}
+				writer.close();
+			}
+			catch (IOException e)
+			{
+				System.out.println("File not found during randomizer function");
+			}
+		}
+		private static String chunkReplace(char s)
+		{
+			switch (s)
+			{
+				case 'n': return randomNames();
+				case 't': return randomTypes();
+
+			}
+			return "ERROR";
+		}
+		private static String randomNames()
+		{
+			String[] names = {"bleeper","munger","thingy"};
+
+			return names[(int)(Math.random() * 2)];
+		}
+		private static String randomTypes()
+		{
+			String[] types = {"String","int","double"};
+			return types[(int)(Math.random() * 2)];
+		}
 
 
 }
---
+++ src/RecursionPreFabs.java
@@ -0,0 +1,14 @@
+
+/* RecursionPreFabs.java
+ * Cameron Connell 
+ * 4/17/19
+ */
+
+public class RecursionPreFabs
+{
+	public static void main(String [] args)
+	{
+
+
+	}
+}
